565179e1fa12a74c477e668d26064b8964fd7850
Johnny Estrada <estradajohnny10@yahoo.com>
2019-11-07 00:09:44 -0800

    Wrote functions

--- stats.c
+++ stats.c
@@ -1,30 +1,50 @@
 #include <stdio.h>
 #include "stats.h"
 
-void print_statistics(){
-
+void print_array(unsigned char* element, unsigned int size){
+    for(unsigned int i = 0; i < size; i++){
+        print("Array Contents:\n")
+        print("Array{%d}: {%d}", i, %element);
+        element++;
+    }
 }
 
-void print_array(int array[], int length){
-
+unsigned char find_median(unsigned char* element, unsigned int size){
+    unsigned char median = 0;
+    unsigned int medianLoc = size / 2;
+    median = element + medianLoc;
+    return median;
 }
 
-int find_median(int array[], int length){
-    
+unsigned char find_mean(unsigned char* element, unsigned int size){
+    unsigned int sum = 0;
+    unsigned char mean = 0;
+    for(unsigned int i = 0; i < size; i++){
+        sum += &element;
+        element++; 
+    }
+    mean = (sum  / size);
+    return mean;
 }
 
-int find_mean(int array[], int length){
-
+unsigned char find_maximum(unsigned char* element, unsigned int size){
+    unsigned char max = 0;
+    for(unsigned int i = 0; i < size; i++){
+        if(&element > max){
+            max = element;
+        }
+        element++;
+    }
+    return max;
 }
 
-int find_maximum(int array[], int length){
-
-}
-
-int find_minimum(int array[], int length){
-
-}
-
-void sort_array(int array[], int length){
-    
+unsigned char find_minimum(unsigned char* element, unsigned int size){
+    unsigned char max = 0;
+    for(unsigned int i = 0; i < size; i++){
+        if(&element > max){
+            max = element;
+        }
+        element++;
+    }
+    return max;
 }
--- stats.h
+++ stats.h
@@ -9,67 +9,67 @@
 
     @return Prints to screen the statistics
 */
-void print_statistics();
+void print_statistics(unigned char median, unsigned char mean, unsigned char max, unsigned char min);
 
 /*
     @brief Prints the contents of an array
 
     @param  array   --  The array being passed
-            length  --  The length of the array
+            size  --  The size of the array
 
     @return Prints the contents of array to the screen
 
 */
-void print_array(int array[], int length);
+void print_array(unsigned char* element, unsigned int size);
 
 /*
     @brief  Finds the median of an Array
 
     @param  array   --  The array being passed
-            length  --  The length of the array
+            size  --  The size of the array
 
     @return The median of the array
 */
-int find_median(int array[], int length);
+unsigned char find_median(unsigned char* element, unsigned int size);
 
 /*
     @brief  Finds the Mean of an Array
 
     @param  array   --  The array being passed
-            length  --  The length of the array
+            size  --  The size of the array
 
     @return The Mean of the array
 */
-int find_mean(int array[], int length);
+unsigned char find_mean(unsigned char* element, unsigned int size);
 
 /*
     @brief  Finds the Maximum of an Array
 
     @param  array   --  The array being passed
-            length  --  The length of the array
+            size  --  The size of the array
 
     @return The maximum of the array
 */
-int find_maximum(int array[], int length);
+unsigned char find_maximum(unsigned char* element, unsigned int size);
 
 /*
     @brief  Finds the Minimum of an Array
 
     @param  array   --  The array being passed
-            length  --  The length of the array
+            size  --  The size of the array
 
     @return The minimum of the array
 */
-int find_minimum(int array[], int length);
+unsigned char find_minimum(unsigned char* element, unsigned int size);
 
 /*
     @brief  Sorts an array from largest to smallest
     
     @param  array   --  The array being passed
-            length  --  The length of the array
+            size  --  The size of the array
 
     @return None
 */
-void sort_array(int array[], int length);
+void sort_array(unsigned char* element, unsigned int size);
 
-#endif /* !FILE_FOO_SEEN */
+#endif /* STATS_H */
