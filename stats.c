fad144fbdcac5561c895102244f6c68b1417bd3c
Johnny Estrada <estradajohnny10@yahoo.com>
2019-12-16 14:41:08 -0800

    Latest revisions

--- stats.c
+++ stats.c
@@ -1,19 +1,39 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <stats.c> 
+ * @brief <Implementation of a few stats for an array>
+ *
+ * @author <Johnny Estrada>
+ * @date <December 16, 2019>
+ *
+ */
 #include <stdio.h>
 #include "stats.h"
 
-#define ARRAY_SIZE 10
+/* Size of the Data Set */
+#define SIZE (40)
 
 void main(){
-    unsigned char arr[ARRAY_SIZE] = {21, 32, 45, 17, 29, 74, 90, 102, 7, 13};
-    unsigned char* p;
-    unsigned char max;
-    p = arr;
+    unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+    unsigned char* p = test;
 
-    // 
-    print_array(p, ARRAY_SIZE);
-    sort_array(p, ARRAY_SIZE);
-    print_array(p, ARRAY_SIZE);
-    print_statistics(p, ARRAY_SIZE);
+    print_statistics(p, SIZE);
+    sort_array(p, SIZE);
+    print_array(p, SIZE);
+    print_statistics(p, SIZE);
     printf("\n");
 }
 
@@ -38,19 +58,26 @@ void print_array(unsigned char* element, unsigned int size){
 }
 
 unsigned char find_median(unsigned char* element, unsigned int size){
-    unsigned char median = 0;
-    unsigned char medianLoc = size / 2;
-    median = (element + medianLoc);
+    unsigned char* medLoc;
+    unsigned char median;
+    // Determine if array is Even or Odd
+    if(size % 2 == 0){
+        medLoc = element + (size / 2);
+        unsigned char num1 = *medLoc;
+        medLoc--;
+        median = (num1 + *medLoc) / 2; 
+    }
+    else{ // median is the number in middle 
+        median = *(element + (size / 2));
+    }
     return median;
 }
 
 unsigned char find_mean(unsigned char* element, unsigned int size){
     unsigned char mean = 0;
-    unsigned char sum = 0;
-
+    unsigned int sum = 0;
     for(unsigned int i = 0; i < size; i++){
-        sum += &element;
-        element++; 
+        sum = sum + *(element + i);
     }
     mean = sum / size;
     return mean;
@@ -70,10 +97,9 @@ unsigned char find_maximum(unsigned char* element, unsigned int size){
 unsigned char find_minimum(unsigned char* element, unsigned int size){
     unsigned char min = *element;
     for(unsigned int i = 0; i < size; i++){
-        if(min < *element){
-            min = *element;
+        if(min > *(element + i)){
+            min = *(element + i);
         }
-        element++;
     }
     return min;
 }
--- stats.h
+++ stats.h
@@ -1,72 +1,95 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <stats.h> 
+ * @brief <Prints the statistics of an array>
+ *         
+ *          Median, Mean, Maximum, Minimum
+ *          as well as sorts an array in descending order
+ * 
+ * @author <Johnny Estrada>
+ * @date <December 16, 2019>
+ *
+ */
+
 #ifndef STATS_H
 #define STATS_H
 
-/*
+/**
     @brief Prints the statistics of an array
     statistics: Median, Mean, Max, Min
 
-    @param None
+    @param element  --  The first element  being passed
+    @param  size  --  The size of the array
 
     @return Prints to screen the statistics
 */
 void print_statistics(unsigned char* element, unsigned int size);
 
-/*
+/**
     @brief Prints the contents of an array
 
-    @param  array   --  The first element  being passed
-            size  --  The size of the array
+    @param  element   --  The first element  being passed
+    @param  size  --  The size of the array
 
     @return Prints the contents of array to the screen
 
 */
 void print_array(unsigned char* element, unsigned int size);
 
-/*
+/**
     @brief  Finds the median of an Array
 
-    @param  array   --  The first element  being passed
-            size  --  The size of the array
+    @param  element   --  The first element  being passed
+    @param  size  --  The size of the array
 
     @return The median of the array
 */
 unsigned char find_median(unsigned char* element, unsigned int size);
 
-/*
+/**
     @brief  Finds the Mean of an Array
 
-    @param  array   --  The first element  being passed
-            size  --  The size of the array
+    @param  element   --  The first element  being passed
+    @param  size  --  The size of the array
 
     @return The Mean of the array
 */
 unsigned char find_mean(unsigned char* element, unsigned int size);
 
-/*
+/**
     @brief  Finds the Maximum of an Array
 
-    @param  array   --  The first element  being passed
-            size  --  The size of the array
+    @param  element --  The first element  being passed
+    @param  size  --  The size of the array
 
     @return The maximum of the array
 */
 unsigned char find_maximum(unsigned char* element, unsigned int size);
 
-/*
+/**
     @brief  Finds the Minimum of an Array
 
-    @param  array   --  The first element  being passed
-            size  --  The size of the array
+    @param  element   --  The first element  being passed
+    @param  size  --  The size of the array
 
     @return The minimum of the array
 */
 unsigned char find_minimum(unsigned char* element, unsigned int size);
 
-/*
+/**
     @brief  Sorts an array from largest to smallest
     
-    @param  array   --  The first element  being passed
-            size  --  The size of the array
+    @param  element  ---  The first element  being passed
+    @param  size  --  The size of the array
 
     @return None
 */
